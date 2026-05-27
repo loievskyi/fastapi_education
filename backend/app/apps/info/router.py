9e1fcc82e76085b07a1b2c9b30ada367edac303c
Slava Loievskyi <loievskyi.slava@gmail.com>
2026-05-27 14:40:14 +0300

    Remove unused `/database_host` endpoint and redundant import

--- backend/app/apps/info/router.py
+++ backend/app/apps/info/router.py
@@ -2,8 +2,6 @@ import socket
 
 from fastapi import APIRouter
 
-from settings import settings
-
 info_router = APIRouter()
 
 
@@ -12,8 +10,3 @@ async def get_backend():
     return {
         "backend": socket.gethostname()
     }
-
-
-@info_router.get("/database_host", tags=["info"], summary="Get the current database hostname")
-async def get_database_hostname():
-    return settings.PGHOST
